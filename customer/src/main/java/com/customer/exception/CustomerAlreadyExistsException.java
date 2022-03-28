c8e429a30e098b08af14f1cf8e71d6531978d340
akank <akank@192.168.1.39>
2022-03-28 17:17:12 +0530

    Exception Handling in spring boot

---
+++ customer/src/main/java/com/customer/exception/CustomerAlreadyExistsException.java
@@ -0,0 +1,18 @@
+//Step 3: Creating custom exception that can be thrown when user tries to add a customer that already exixts
+package com.customer.exception;
+
+public class CustomerAlreadyExistsException extends RuntimeException {
+
+	private String message;
+
+	public CustomerAlreadyExistsException() {
+
+	}
+
+	public CustomerAlreadyExistsException(String msg) {
+		super(msg);
+		this.message = msg;
+
+	}
+
+}
---
+++ customer/src/main/java/com/customer/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+//Step 2: Creating a Repository Interface extending JpaRepository
+package com.customer.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.customer.model.Customer;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
